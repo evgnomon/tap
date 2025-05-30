89fbab8debbda2b1d6185969d912abe5a9703789
Hamed Ghasemzadeh <hg@evgnomon.org>
2025-05-30 12:50:24 +0200

    Fix linting issues

--- src/shared/pages/Home.tsx
+++ src/shared/pages/Home.tsx
@@ -1,8 +1,6 @@
 import React from "react";
-import { useTheme } from "@shared/context/ThemeContext";
 
 const Home: React.FC = () => {
-  const { theme } = useTheme();
   return (
     <div className="rounded-full px-2 py-1.5 text-sm/6">
       <div className="h-20"></div>
